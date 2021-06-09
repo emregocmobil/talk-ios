4ef4feef0ef31ce4105b6009570ed8bdcf08c2fc
Ivan Sein <ivan@nextcloud.com>
2021-06-09 18:54:02 +0200

    Fix capability check (Changed for testing).
    Signed-off-by: Ivan Sein <ivan@nextcloud.com>

--- NextcloudTalk/NCChatViewController.m
+++ NextcloudTalk/NCChatViewController.m
@@ -1244,7 +1244,7 @@ NSString * const NCChatViewControllerTalkToUserNotification = @"NCChatViewContro
 {
     BOOL canPress = [super canPressRightButton];
     
-    if (!canPress && [[NCDatabaseManager sharedInstance] serverHasTalkCapability:kCapabilitySystemMessages]) {
+    if (!canPress && [[NCDatabaseManager sharedInstance] serverHasTalkCapability:kCapabilityVoiceMessage]) {
         [self showVoiceMessageRecordButton];
         return YES;
     }
