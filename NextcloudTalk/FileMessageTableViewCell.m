30e9c36c409e8bc0dcfd4819266fab162e3df649
Ivan Sein <ivan@nextcloud.com>
2021-11-22 18:31:28 +0100

    Set contact name as parsedMessage if there is one.
    Signed-off-by: Ivan Sein <ivan@nextcloud.com>

--- NextcloudTalk/FileMessageTableViewCell.m
+++ NextcloudTalk/FileMessageTableViewCell.m
@@ -490,9 +490,6 @@
     
     self.fileParameter = message.file;
     
-    if (message.file.contactName) {
-        self.bodyTextView.text = message.file.contactName;
-    }
     if (message.file.contactPhotoImage) {
         [self.previewImageView setImage:message.file.contactPhotoImage];
     }
