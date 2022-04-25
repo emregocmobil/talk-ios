2b893ceefa664723287d0ebb0559b7b762c91b69
Ivan Sein <ivan@nextcloud.com>
2022-04-26 01:31:43 +0200

    Add item type selector and fetch items by type.
    Signed-off-by: Ivan Sein <ivan@nextcloud.com>

--- NextcloudTalk/RoomSharedItemsTableViewController.swift
+++ NextcloudTalk/RoomSharedItemsTableViewController.swift
@@ -49,6 +49,15 @@ import QuickLook
         fatalError("init(coder:) has not been implemented")
     }
 
+    init(roomToken: String) {
+        self.roomToken = roomToken
+        super.init(nibName: "RoomSharedItemsTableViewController", bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
