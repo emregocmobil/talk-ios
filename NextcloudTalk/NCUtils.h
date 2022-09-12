cb0fefb4cace7c012f8cd42f630d6bfaf7dfa689
Marcel Müller <marcel-mueller@gmx.de>
2022-09-12 15:51:38 +0200

    Move getVersionString to NCAppBranding and allow alternate app version
    Signed-off-by: Marcel Müller <marcel-mueller@gmx.de>

--- NextcloudTalk/NCUtils.h
+++ NextcloudTalk/NCUtils.h
@@ -33,8 +33,6 @@ NS_ASSUME_NONNULL_BEGIN
 + (BOOL)isVideoFileType:(NSString *)fileMIMEType;
 
 + (BOOL)isNextcloudAppInstalled;
-+ (BOOL)isBetaVersion;
-+ (NSString *)getVersionString;
 + (void)openFileInNextcloudApp:(NSString *)path withFileLink:(NSString *)link;
 + (void)openFileInNextcloudAppOrBrowser:(NSString *)path withFileLink:(NSString *)link;
 + (void)openLinkInBrowser:(NSString *)link;
