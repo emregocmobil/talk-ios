3234305fb996394cc56776bd20e471695764398b
Marcel Müller <marcel-mueller@gmx.de>
2022-10-15 18:56:27 +0200

    Wait for animation to complete before showing a chatViewController Fix highlighting of rooms when shown from a push notification
    Signed-off-by: Marcel Müller <marcel-mueller@gmx.de>

--- NextcloudTalk/RoomsTableViewController.m
+++ NextcloudTalk/RoomsTableViewController.m
@@ -1299,7 +1299,6 @@ typedef void (^FetchRoomsCompletionBlock)(BOOL success);
     
     // Present room chat
     [self presentChatForRoomAtIndexPath:indexPath];
-    [self setSelectedRoomToken:[self roomForIndexPath:indexPath].token];
 }
 
 - (void)setSelectedRoomToken:(NSString *)selectedRoomToken
