1d4c9f9a2c1a5d4346d1fd1209b51d5b8730df2f
Marcel Müller <marcel-mueller@gmx.de>
2022-11-28 13:58:31 +0100

    Remove logging of updateRooms
    Signed-off-by: Marcel Müller <marcel-mueller@gmx.de>

--- NextcloudTalk/NCRoomsManager.m
+++ NextcloudTalk/NCRoomsManager.m
@@ -491,8 +491,6 @@ static NSInteger kNotJoiningAnymoreStatusCode = 999;
 
 - (void)updateRoomsUpdatingUserStatus:(BOOL)updateStatus withCompletionBlock:(UpdateRoomsCompletionBlock)block
 {
-    [NCUtils log:@"Start updateRoomsUpdatingUserStatus"];
-
     TalkAccount *activeAccount = [[NCDatabaseManager sharedInstance] activeAccount];
     [[NCAPIController sharedInstance] getRoomsForAccount:activeAccount updateStatus:updateStatus withCompletionBlock:^(NSArray *rooms, NSError *error, NSInteger statusCode) {
         NSMutableDictionary *userInfo = [NSMutableDictionary new];
@@ -537,8 +535,6 @@ static NSInteger kNotJoiningAnymoreStatusCode = 999;
         if (block) {
             block(roomsWithNewMessages, error);
         }
-
-        [NCUtils log:@"End getRoomsForAccount completionBlock"];
     }];
 }
 
