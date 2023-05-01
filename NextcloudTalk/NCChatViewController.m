11e3178bac6ce9f044659bbbc25fa09b8122c63a
Marcel Müller <marcel-mueller@gmx.de>
2023-05-01 12:25:18 +0200

    Send stoppedTyping when we move to the background
    Signed-off-by: Marcel Müller <marcel-mueller@gmx.de>

--- NextcloudTalk/NCChatViewController.m
+++ NextcloudTalk/NCChatViewController.m
@@ -673,6 +673,7 @@ NSString * const NCChatViewControllerTalkToUserNotification = @"NCChatViewContro
     [self savePendingMessage];
     [_chatController stopChatController];
     [_messageExpirationTimer invalidate];
+    [self stopTyping];
     [[NCRoomsManager sharedInstance] leaveChatInRoom:_room.token];
 }
 
