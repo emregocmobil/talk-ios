76e5aacab36b1132a5111b6a5f4f03d728daca30
Ivan Sein <ivan@nextcloud.com>
2023-07-13 18:25:39 +0200

    Check that the view controller to present is not being presented.
    Signed-off-by: Ivan Sein <ivan@nextcloud.com>

--- NextcloudTalk/NCSplitViewController.swift
+++ NextcloudTalk/NCSplitViewController.swift
@@ -67,7 +67,9 @@
 
     override func present(_ viewControllerToPresent: UIViewController, animated flag: Bool, completion: (() -> Void)? = nil) {
         self.internalExecuteAfterTransition {
-            super.present(viewControllerToPresent, animated: flag, completion: completion)
+            if !viewControllerToPresent.isBeingPresented {
+                super.present(viewControllerToPresent, animated: flag, completion: completion)
+            }
         }
     }
 
